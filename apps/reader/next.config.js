1aebfcf6d51a8d3930679367c0033ced554a5c45
Zhichen <qhwuzhichen@126.com>
2024-12-18 23:56:51 +0800

    Update next.config.js

--- apps/reader/next.config.js
+++ apps/reader/next.config.js
@@ -1,35 +1,19 @@
-const path = require('path')
+const path = require('path');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
-const { withSentryConfig } = require('@sentry/nextjs')
+});
 const withPWA = require('next-pwa')({
   dest: 'public',
-})
+});
 const withTM = require('next-transpile-modules')([
   '@flow/internal',
   '@flow/epubjs',
   '@material/material-color-utilities',
-])
+]);
 
-const IS_DEV = process.env.NODE_ENV === 'development'
-const IS_DOCKER = process.env.DOCKER
-
-/**
- * @type {import('@sentry/nextjs').SentryWebpackPluginOptions}
- **/
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+const IS_DEV = process.env.NODE_ENV === 'development';
+const IS_DOCKER = process.env.DOCKER;
 
 /**
  * @type {import('next').NextConfig}
@@ -37,7 +21,7 @@ const sentryWebpackPluginOptions = {
 const config = {
   pageExtensions: ['ts', 'tsx'],
   webpack(config) {
-    return config
+    return config;
   },
   i18n: {
     locales: ['en-US', 'zh-CN'],
@@ -49,17 +33,8 @@ const config = {
       outputFileTracingRoot: path.join(__dirname, '../../'),
     },
   }),
-}
+};
 
-const base = withPWA(withTM(withBundleAnalyzer(config)))
+const base = withPWA(withTM(withBundleAnalyzer(config)));
 
-const dev = base
-const docker = base
-const prod = withSentryConfig(
-  base,
-  // Make sure adding Sentry options is the last code to run before exporting, to
-  // ensure that your source maps include changes from all other Webpack plugins
-  sentryWebpackPluginOptions,
-)
-
-module.exports = IS_DEV ? dev : IS_DOCKER ? docker : prod
+module.exports = IS_DEV ? base : IS_DOCKER ? base : base;
